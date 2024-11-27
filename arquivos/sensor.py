68847c68113a7bb27b89467f13af2fba8c205390
Denise Tavares <denise.nascmento@gmail.com>
2024-11-27 15:37:59 -0300

    Update sensor.py

--- arquivos/sensor.py
+++ arquivos/sensor.py
@@ -1,38 +1,59 @@
-import subprocess
 import serial
+import datetime
+import subprocess
 from time import sleep
+import sys
 
-def bloquearPc():
+def cronometro_regressivo(segundos):
+    
+    start_time = datetime.datetime.now()
+
+    while segundos > 0:
+        tempo_restante = datetime.timedelta(seconds=segundos)
+        print(f"TEMPO RESTANTE: {tempo_restante}.")
+        print()
+        sleep(1)
+        segundos -= 1
+
+    print("TEMPO ESGOTADO!")
+    sleep(0.5)
     subprocess.run("rundll32.exe user32.dll,LockWorkStation", shell=True)
+    sys.exit()
+
 
 def distancia(dist):
+
     if dist > 80:
         print("Longe")
         sleep(0.5)
-        bloquearPc()
+        cronometro_regressivo(30)
         return 1
     else:
         print("Perto")
         return 0
 
-print('Inicio do codigo')
-while True: #Loop para a conexão com o Arduino
-    try:  #Tenta se conectar, se conseguir, o loop se encerra
-        arduino = serial.Serial('COM10', 9600)
-        print('2')
-        print('Arduino conectado')
-        break
-    except Exception as e: print(e)
-        # pass
-print('Depois do while true')
-while True: #Loop principal
-    msg = str(arduino.readline()) #Lê os dados em formato de string
-    msg = msg[21:-8] #Fatia a string
-    msg = float(msg)
-    #print("Mensagem: {:.2f}".format(msg)) #Imprime a mensagem
-    #msg = float(msg)
-    distancia(msg)
-    if distancia(msg) == 1:
-        break
-    #print(msg,"cm.")
-    arduino.flush() #Limpa a comunicação
+
+print("Inicio do Projeto!")
+
+def main():
+
+    while True:
+        try:  
+            arduino = serial.Serial('COM10', 9600)
+            print('Arduino CONECTADO!')
+            break
+        except Exception as e: 
+            print(e)
+            sys.exit()
+    
+    while True:
+        msg = str(arduino.readline())
+        msg = msg[21:-8] 
+        msg = float(msg)
+        distancia(msg)
+        if distancia(msg) == 1:
+            sys.exit()
+        arduino.flush()
+
+
+main()
