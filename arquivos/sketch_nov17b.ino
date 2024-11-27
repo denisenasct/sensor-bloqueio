c26b4596581a165e2d2c683009ee2eae6df2aad5
Denise Tavares <denise.nascmento@gmail.com>
2024-11-27 14:55:29 -0300

    Update sketch_nov17b.ino

--- arquivos/sketch_nov17b.ino
+++ arquivos/sketch_nov17b.ino
@@ -1,35 +1,47 @@
-const int trigPin = 5; // Emite o sinal sonoro
-const int echoPin = 4; // Capta as ondas sonoras refletidas
+//CÓDIGO SENSOR ULTRASSONICO PRO ARDUINO:
+
+const int trigPin = 5; //Emite o sinal sonoro
+const int echoPin = 4; //Capta as ondas sonoras refletidas
 int enviarpy;
 
 void setup() {
+  // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
+  pinMode(trigPin, OUTPUT); //Definido Como saida pois ele emite o sinal sonoro
+  pinMode(echoPin, INPUT); //Definido como entrada pois capta as ondas sonoras refletidas
 }
 
 void loop() {
+  // put your main code here, to run repeatedly:
+
+  //Enviando pulso ultrassonico:
   digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
+  delayMicroseconds(1000);
   digitalWrite(trigPin, HIGH);
-  delayMicroseconds(5);
-  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2); 
+  //digitalWrite(trigPin, LOW);
 
+  //Leitura do tempo de retorno do pulso:
   long duracao = pulseIn(echoPin, HIGH);
-  float distancia = duracao * 0.034 / 2;
 
+  //Calcula a distancia em cm:
+  float distancia = duracao * 0.34/2;
+  
   if (distancia > 80) {
-    enviarpy = 1;
+    int enviarpy = 1 ;
     Serial.print(enviarpy);
-  } else {
-    enviarpy = 0;
+  }
+  else {
+    int enviarpy = 0;
     Serial.print(enviarpy);
   }
 
+
+
+  //Imprime a distancia no monitor serial:
   Serial.print("Distância: ");
   Serial.print(distancia);
-  Serial.println("cm.");
 
-  delay(500);
-} 
-  
+  delay(500); // Pequena pausa antes da próxima leitura
+
+}
